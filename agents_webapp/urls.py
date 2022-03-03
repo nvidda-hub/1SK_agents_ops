e3e17c535adc14a99d0bea0087bb25a852b31f0c
nvidda-hub <nkvidda@gmail.com>
2022-03-03 23:21:06 +0530

    login page frontend done

---
+++ agents_webapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from agents_webapp import views
+from agents_webapp.forms import LoginForm
+from django.contrib.auth import views as auth_views
+
+app_name = "agents_webapp"
+
+urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='agents_webapp/login.html', authentication_form=LoginForm), name='login'),
+]
